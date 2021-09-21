e02302ab28f120c0df3673e25f36ffdad4476b8b
Chirag0104 <chiraggoyal0104@gmail.com>
2021-09-21 23:17:52 +0530

    Update divisiblesumpairs.cpp

--- divisiblesumpairs.cpp
+++ divisiblesumpairs.cpp
@@ -3,14 +3,14 @@ using namespace std;
 int main(){
     int n,k;
     cin>>n>>k;
-    int arr[n];
+    int arr1[n];
     for(int i=0;i<n;i++){
-        cin>>arr[i];
+        cin>>arr1[i];
     }
     int counter=0;
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            int b = arr[i]+arr[j];
+            int b = arr1[i]+arr1[j];
             if(i<j){
             if(b == k || b%k == 0 ){
                 counter++;
