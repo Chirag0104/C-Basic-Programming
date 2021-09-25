193e5e728b6b70e7da118ee46b13cf1e074f0291
Chirag0104 <chiraggoyal0104@gmail.com>
2021-09-25 23:07:20 +0530

    Create savetheprisoner.cpp

---
+++ savetheprisoner.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t,n,m,s,count;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        cin>>n>>m>>s;
+        for(int i=s;i<s+m;i++){
+            if(i>=n){
+                i=1;
+                
+            }
+            count=i;
+            
+            
+        }
+        cout<<count<<endl;
+        
+    }
+}
